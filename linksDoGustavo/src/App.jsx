2d6f6eea6806d57194ca96ff2258b92ade88b1f2
Gustavo <gusssoares09@gmail.com>
2025-04-07 20:22:41 -0300

    quase lá

--- linksDoGustavo/src/App.jsx
+++ linksDoGustavo/src/App.jsx
@@ -14,22 +14,22 @@ function App() {
         <img className='FotoGustavo'src="Images/gustavoFoto.png" alt="" />
       <h1>Gustavo Franco</h1>
       <p>Aqui possui alguns Contatos e Projetos meus</p>
-        <a className='texto-botao' href="">
+        <a className='texto-botao' href="https://github.com/Gucrima">
         <button className='botao-dos-links2'> <TbBrandGithubFilled className='icon-git'/>GitHub</button>
         </a>
-        <a className='texto-botao' href="">
+        <a className='texto-botao' href="https://www.linkedin.com/in/gustavo-franco-83b4022a7/">
         <button className='botao-dos-links3'> <FaLinkedin className='icon-git' /> Linkdln</button>
         </a>
-        <a className='texto-botao' href="">
+        <a className='texto-botao' href="https://www.instagram.com/gustavo.sfranco/">
         <button className='botao-dos-links4'> <FaInstagram className='icon-git' /> Instagram</button>
         </a>
-        <a className='texto-botao' href="">
+        <a className='texto-botao' href="mailto:gusssoares09@gmail.com?subject=olá">
         <button className='botao-dos-links5'> <BiLogoGmail className='icon-git' /> Gmail</button>
         </a>
-        <a className='texto-botao' href="">
+        <a className='texto-botao' href="https://open.spotify.com/user/z846rwhf4c886bri8p69spqph?si=6e2978dccbc64ed8">
         <button className='botao-dos-links6'> <FaSpotify className='icon-git' /> Spotify</button>
         </a>
-        <a className='texto-botao' href="">
+        <a className='texto-botao' href="https://www.jw.org/pt/testemunhas-de-jeova/">
         <button className='botao-dos-links'>JW.org</button>
         </a>
       </div>
