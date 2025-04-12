29029be2421a51043426dac55f72390ce66c974b
Gustavo <gusssoares09@gmail.com>
2025-04-11 21:14:32 -0300

    dando aquela tapiada no css mas da ainda para melhorar... né rafa

--- linksDoGustavo/src/App.jsx
+++ linksDoGustavo/src/App.jsx
@@ -1,38 +1,12 @@
 import './App.css'
-import { TbBrandGithubFilled } from "react-icons/tb";
-import { FaLinkedin } from "react-icons/fa";
-import { FaInstagram } from "react-icons/fa";
-import { BiLogoGmail } from "react-icons/bi";
-import { FaSpotify } from "react-icons/fa";
-
+import FrontComOsLinks from './components/FrontComOsLinks'
 
 function App() {
  
 
   return (
-    <div className="fundo-dos-links">
-      <div className="conteiner-links">
-        <img className='FotoGustavo'src="Images/gustavoFoto.png" alt="" />
-      <h1>Gustavo Franco</h1>
-        <a className='texto-botao' href="https://github.com/Gucrima">
-        <button className='botao-dos-links2'> <TbBrandGithubFilled className='icon-git'/>GitHub</button>
-        </a>
-        <a className='texto-botao' href="https://www.linkedin.com/in/gustavo-franco-83b4022a7/">
-        <button className='botao-dos-links3'> <FaLinkedin className='icon-git' /> Linkdln</button>
-        </a>
-        <a className='texto-botao' href="https://www.instagram.com/gustavo.sfranco/">
-        <button className='botao-dos-links4'> <FaInstagram className='icon-git' /> Instagram</button>
-        </a>
-        <a className='texto-botao' href="mailto:gusssoares09@gmail.com?subject=olá">
-        <button className='botao-dos-links5'> <BiLogoGmail className='icon-git' /> Gmail</button>
-        </a>
-        <a className='texto-botao' href="https://open.spotify.com/user/z846rwhf4c886bri8p69spqph?si=6e2978dccbc64ed8">
-        <button className='botao-dos-links6'> <FaSpotify className='icon-git' /> Spotify</button>
-        </a>
-        <a className='texto-botao' href="https://www.jw.org/pt/testemunhas-de-jeova/">
-        <button className='botao-dos-links'>JW.org</button>
-        </a>
-      </div>
+    <div>
+      <FrontComOsLinks />
     </div>
   )
 }
---
+++ linksDoGustavo/src/components/FrontComOsLinks.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import './FrontComOsLinks.css'
+import { TbBrandGithubFilled } from "react-icons/tb";
+import { FaLinkedin } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { BiLogoGmail } from "react-icons/bi";
+import { FaSpotify } from "react-icons/fa";
+
+function FrontComOsLinks() {
+  return (
+    <div className='container'>
+      <div className="conteiner-links">
+        <img className='FotoGustavo'src="Images/gustavoFoto.png" alt="" />
+      <h1>Gustavo Franco</h1>
+        <a className='texto-botao' href="https://github.com/Gucrima">
+        <button className='botao-dos-links1'> <TbBrandGithubFilled className='icon-git'/>GitHub</button>
+        </a>
+        <a className='texto-botao' href="https://www.linkedin.com/in/gustavo-franco-83b4022a7/">
+        <button className='botao-dos-links2'> <FaLinkedin className='icon-git' /> Linkdln</button>
+        </a>
+        <a className='texto-botao' href="https://www.instagram.com/gustavo.sfranco/">
+        <button className='botao-dos-links3'> <FaInstagram className='icon-git' /> Instagram</button>
+        </a>
+        <a className='texto-botao' href="mailto:gusssoares09@gmail.com?subject=olá">
+        <button className='botao-dos-links4'> <BiLogoGmail className='icon-insta' /> Gmail</button>
+        </a>
+        <a className='texto-botao' href="https://open.spotify.com/user/z846rwhf4c886bri8p69spqph?si=6e2978dccbc64ed8">
+        <button className='botao-dos-links5'> <FaSpotify className='icon-git' /> Spotify</button>
+        </a>
+        <a className='texto-botao' href="https://www.jw.org/pt/testemunhas-de-jeova/">
+        <button className='botao-dos-links6'>JW.org</button>
+        </a>
+      </div>
+    </div>
+  )
+}
+
+export default FrontComOsLinks
