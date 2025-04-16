d13866cd898e00961756d46b095c8436a2317a7a
Gustavo <gusssoares09@gmail.com>
2025-04-16 09:09:32 -0300

    add link dos repositório de C#

--- linksDoGustavo/src/components/FrontComOsLinks.jsx
+++ linksDoGustavo/src/components/FrontComOsLinks.jsx
@@ -5,6 +5,7 @@ import { FaLinkedin } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 import { BiLogoGmail } from "react-icons/bi";
 import { FaSpotify } from "react-icons/fa";
+import { PiFileCSharp } from "react-icons/pi";
 
 function FrontComOsLinks() {
   return (
@@ -12,6 +13,9 @@ function FrontComOsLinks() {
       <div className="conteiner-links">
         <img className='FotoGustavo'src="Images/gustavoFoto.png" alt="" />
       <h1>Gustavo Franco</h1>
+        <a className='texto-botao' href="https://www.jw.org/pt/testemunhas-de-jeova/">
+        <button className='botao-dos-links6'>JW.org</button>
+        </a>
         <a className='texto-botao' href="https://github.com/Gucrima">
         <button className='botao-dos-links1'> <TbBrandGithubFilled className='icon-git'/>GitHub</button>
         </a>
@@ -22,13 +26,13 @@ function FrontComOsLinks() {
         <button className='botao-dos-links3'> <FaInstagram className='icon-git' /> Instagram</button>
         </a>
         <a className='texto-botao' href="mailto:gusssoares09@gmail.com?subject=olá">
-        <button className='botao-dos-links4'> <BiLogoGmail className='icon-insta' /> Gmail</button>
+        <button className='botao-dos-links4'> <BiLogoGmail className='icon-git' /> Gmail</button>
         </a>
         <a className='texto-botao' href="https://open.spotify.com/user/z846rwhf4c886bri8p69spqph?si=6e2978dccbc64ed8">
         <button className='botao-dos-links5'> <FaSpotify className='icon-git' /> Spotify</button>
         </a>
-        <a className='texto-botao' href="https://www.jw.org/pt/testemunhas-de-jeova/">
-        <button className='botao-dos-links6'>JW.org</button>
+        <a className='texto-botao' href="https://github.com/Gucrima/PrimeirosProjetosEmCS">
+        <button className='botao-dos-links7'> Repositório de <PiFileCSharp className='icon-cs' /></button>
         </a>
       </div>
     </div>
